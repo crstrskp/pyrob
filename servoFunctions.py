70d75da667dafecb74cfc1dda683d1e1fbfae010
Luffe <jespertjorne@gmail.com>
2020-01-29 20:45:30 +0100

    implemented min / max for servo values

--- servoFunctions.py
+++ servoFunctions.py
@@ -27,8 +27,8 @@ class servoFunctions(object):
         if self.pin1 != 0:
             self.pi.set_mode(self.pin1, pigpio.OUTPUT)
             self.servo1Angle = 90
-            self.servo1MaxAngle = 125
-            self.servo1MinAngle = 85
+            self.servo1MaxAngle = 150
+            self.servo1MinAngle = 30
          
         if self.pin2 != 0:
             self.pi.set_mode(self.pin2, pigpio.OUTPUT)
@@ -67,32 +67,59 @@ class servoFunctions(object):
             
         self.SetServoAngle(angle, servo)
             
-    def SetServoAngle(self, angle, servo):
         
-        pulsewidth = (angle * 11.1111) + 500
-
+    def SetServoAngle(self, a, servo):
         if servo == 1:
             pin = self.pin1
+            if a > self.servo1MaxAngle:
+                angle = self.servo1MaxAngle
+            elif a < self.servo1MinAngle:
+                angle = self.servo1MinAngle
+            else:
+                angle = a
         if servo == 2:
             pin = self.pin2
+            if a > self.servo2MaxAngle:
+                angle = self.servo2MaxAngle
+            elif a < self.servo2MinAngle:
+                angle = self.servo2MinAngle
+            else:
+                angle = a
         if servo == 3:
             pin = self.pin3
+            if a > self.servo3MaxAngle:
+                angle = self.servo3MaxAngle
+            elif a < self.servo3MinAngle:
+                angle = self.servo3MinAngle
+            else:
+                angle = a
         if servo == 4:
             pin = self.pin4
-            
+            if a > self.servo4MaxAngle:
+                angle = self.servo4MaxAngle
+            elif a < self.servo4MinAngle:
+                angle = self.servo4MinAngle
+            else:
+                angle = a
+
+
+        pulsewidth = (angle * 11.1111) + 500
+        print(angle)
+        print(pulsewidth)
+        
         self.pi.set_servo_pulsewidth(pin, pulsewidth)
         self.UpdateAngles()
 
 
     def UpdateAngles(self):
         if hasattr(self, "servo1Angle"):
-            self.servo1Angle = self.get_servo_pulsewidth(self.pin1)
+            self.servo1Angle = self.pi.get_servo_pulsewidth(self.pin1)
         if hasattr(self, "servo2Angle"):
-            self.servo2Angle = self.get_servo_pulsewidth(self.pin2)
+            self.servo2Angle = self.pi.get_servo_pulsewidth(self.pin2)
         if hasattr(self, "servo3Angle"):
-            self.servo3Angle = self.get_servo_pulsewidth(self.pin3)
+            self.servo3Angle = self.pi.get_servo_pulsewidth(self.pin3)
         if hasattr(self, "servo4Angle"):
-            self.servo4Angle = self.get_servo_pulsewidth(self.pin4)
+            self.servo4Angle = self.pi.get_servo_pulsewidth(self.pin4)
         
         
     def StopPigpio(self):
